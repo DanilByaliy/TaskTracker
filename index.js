11f54feeb5eac8c46d1ac371bd158f0e82887ea9
DanilByaliy <byaliy03@gmail.com>
2022-05-27 23:44:22 +0300

    Add some changes
    Changed the regular expression, now it includes more acceptable options.
    Added date formatting, this is required to parse the date correctly.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ try {
 }
 
 function addTask(title, description, deadline) {
-  const regexp = /^\d{4}-\d{2}-\d{2}$/;
+  const regexp = /^\d{4}-\d{2}-\d{2}( \d{2})?(:\d{2})?$/;
   if (!regexp.test(deadline)) {
     console.log('Wrong format');
     return;
@@ -17,7 +17,7 @@ function addTask(title, description, deadline) {
   const task = {
     title: title,
     description: description,
-    deadline: deadline,
+    deadline: deadline.replace(' ', 'T') + ':00',
     isDone: false
   }
   tasksArr.push(task);
