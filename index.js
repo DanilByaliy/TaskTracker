ef7693d9926a27d7852b61726272f2ce1161e06e
Ivan Labiak <labyak.ivan2003@gmail.com>
2022-05-27 21:40:50 +0300

    modified date output and made feature to show all tasks

--- index.js
+++ index.js
@@ -1,51 +1,72 @@
-'use strict';
+"use strict";
 
-const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+const argv = require("yargs/yargs")(process.argv.slice(2)).argv;
 
-const tasksArr = require('./tasksfile.json')
+const tasksArr = require("./tasksfile.json");
 
 const readArgs = (args) => {
-    if(args.s){
-        console.log('1')
-    }
-
-    console.dir(args)
-
-}
+  if (args.s) {
+    console.log("Список невиконаних завдань:");
+    showTasks();
+  } else if (args.showall) {
+    console.log("Список всіх завдань:");
+    tasksOutput(tasksArr);
+  }
+};
 
 const showTasks = () => {
-    let undoneTasks = [];
-    for(let el of tasksArr){
-        if(el.taskDone === false){
-            undoneTasks.push(el)
-        }
+  let undoneTasks = [];
+  for (let el of tasksArr) {
+    if (el.taskDone === false) {
+      undoneTasks.push(el);
     }
-    tasksOutput(undoneTasks)
-}
+  }
+  tasksOutput(undoneTasks);
+};
 
 const tasksOutput = (tasksArray) => {
-    let counter = 1;
-    let outputStr = ''
-    for(let el of tasksArray){
-        outputStr += `\nЗавдання №${counter}
+  let counter = 1;
+  let outputStr = "";
+  for (let el of tasksArray) {
+    outputStr += `\nЗавдання №${counter}
 Назва: ${el.taskName}
-`
-if(el.taskDetails){
-    outputStr += `Опис: ${el.taskDetails}\n`
-}
-if(el.taskDeadline){
-    outputStr += `Дедлайн: ${el.taskDeadline}\n`
-}
-if(el.taskDone){
-    outputStr += `Стан завдання: ✓\n`
-}else{
-    outputStr += `Стан завдання: ✗\n`
-}
-counter++;
+`;
+    if (el.taskDetails) {
+      outputStr += `Опис: ${el.taskDetails}\n`;
     }
-    console.log(outputStr)
-} 
+    if (el.taskDeadline) {
+      outputStr += getDateString(el.taskDeadline);
+    }
+    if (el.taskDone) {
+      outputStr += `Стан завдання: ✓\n`;
+    } else {
+      outputStr += `Стан завдання: ✗\n`;
+    }
+    counter++;
+  }
+  console.log(outputStr);
+};
 
-showTasks()
+const getDateString = (dateStr) => {
+  let date = new Date(dateStr);
+  let res =
+    "Дедлайн: " +
+    date.getDate() +
+    "/" +
+    (date.getMonth() + 1) +
+    "/" +
+    date.getFullYear();
+  // check if date has hours
+  if (
+    Date.parse(date)
+      .toString()
+      .match(/[0-9]*000000/)
+  ) {
+    res += "\n";
+  } else {
+    res += " " + date.getHours() + ":" + date.getMinutes() + "\n";
+  }
+  return res;
+};
 
-//readArgs(argv)
+readArgs(argv);
