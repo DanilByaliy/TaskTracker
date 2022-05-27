6f5e50682a61216575a2928b04132a1d19f20ad2
DanilByaliy <byaliy03@gmail.com>
2022-05-27 23:59:47 +0300

    Add a task editing feature
    The function checks the presence of arguments, if any, then change the
    corresponding fields of the desired task, after which the updated array
    is written to a file.

--- index.js
+++ index.js
@@ -23,3 +23,19 @@ function addTask(title, description, deadline) {
   tasksArr.push(task);
   fs.writeFileSync('tasksfile.json', `${JSON.stringify(tasksArr)}`);
 }
+
+function editTask(id, title, description, deadline){
+  const regexp = /^\d{4}-\d{2}-\d{2}( \d{2})?(:\d{2})?$/;
+  
+  title ? tasksArr[id].title = title : null;
+  description ? tasksArr[id].description = description : null;
+  if (deadline) {
+    if (!regexp.test(deadline)) {
+    console.log('Wrong format');
+    return;
+    }
+    tasksArr[id].deadline = deadline.replace(' ', 'T') + ':00';
+  }
+
+  fs.writeFileSync('tasksfile.json', `${JSON.stringify(tasksArr)}`);
+}
